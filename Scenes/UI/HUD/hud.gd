fa202334397f0d7628833f4a09419d489c011d6e
Christopher Glenn <topherblair@gmail.com>
2024-08-19 23:39:19 -0700

    8/19/2024 MIDNIGHT

---
+++ Scenes/UI/HUD/hud.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+# GAME HUD
+# the heads up display for the game
+# will differ in the information that it displays depending on the character class
+
+
+func _ready():
+    self.visible = true # DEBUGGING show the HUD
