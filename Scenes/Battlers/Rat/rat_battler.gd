876917f1cd7b8be5b4f3f475bd2ab8130eb4689b
Christopher Glenn <topherblair@gmail.com>
2024-08-24 23:15:21 -0700

    8/24/2024 MIDNIGHT

--- Scenes/Battlers/Rat/rat_battler.gd
+++ Scenes/Battlers/Rat/rat_battler.gd
@@ -2,6 +2,8 @@ extends Node2D
 # THE RAT!!
 # a rat enemy that just does basic attacks and blocks some incoming attacks
 # this is the first enemy of the game and does minimal damage
+@onready var RAT_ANIM = $Animation_Rat
+@onready var RNG = RandomNumberGenerator.new()
 @export var hit_points : int = 50 # rat's hit points
 @export var defense : int = 20 # rat's defense points
 @export var rat_state_timer : int = 60 # every time this hits zero the rat will check and see it's next move
@@ -9,7 +11,7 @@ var RAT_STATE : String = "IDLE" # IDLE, HIT, ATTACK, DEAD, BLOCK
 
 
 func _ready():
-    pass
+    RNG.randomize() # seed random
 
 func _process(delta):
     rat_ai(delta)
@@ -21,4 +23,6 @@ func rat_ai(_clock):
     # what the next best move is. The rat will also respond, possibly, to the player's attacks based on whether they
     # were just hit. The rat is a 'dumb' enemy so it's blocking isn't really refined.
     # check the rat state 
-    pass
+    if RAT_STATE == "IDLE":
+        # the rat is idle meaning it's waiting for the player to move
+        RAT_ANIM.play("idle") # play the idle animation
