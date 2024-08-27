53298948cf5f7fbfd6fd3d50be4897fac0bacc80
Christopher Glenn <topherblair@gmail.com>
2024-08-27 11:14:36 -0700

    8/27/2024 NOON

--- Scenes/Battlers/Fighter_Battler/fighter_battler.gd
+++ Scenes/Battlers/Fighter_Battler/fighter_battler.gd
@@ -2,14 +2,16 @@ extends Node2D
 # FIGHTER BATTLER SCRIPT
 # if the player is the fighter class then this is the battler for them!
 @onready var FIGHTER_ANIM = $Animation_Fighter
+@export var cool_down : int = 60 # cool down period between movements
 var FIGHTER_STATE : String = "IDLE" # IDLE, HIT, ATTACK, BLOCK, DEAD, DODGE LEFT, DODGE RIGHT, FATIGUED, CASTING, ITEM
+var cool_down_rec # keeps track of cool_down
 
 
 func _ready():
-    pass
+    cool_down_rec = cool_down # record the cool_down setting
 
-func _process(_delta):
-    pass
+func _process(delta):
+    fighter_control(delta) # fighter control function
 
 
 func fighter_control(_clock):
@@ -17,7 +19,7 @@ func fighter_control(_clock):
     # the fighter will stay idle until player input is detected or the player is hit or killed
     if FIGHTER_STATE == "IDLE":
         # idle state ideally awaiting user input...
-        pass
+        FIGHTER_ANIM.play("idle")
     elif FIGHTER_STATE == "HIT":
         pass
     elif FIGHTER_STATE == "FATIGUED":
