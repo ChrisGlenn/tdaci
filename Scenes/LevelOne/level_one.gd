1dd335ee8b67377753e37ff5da4b4e840b46a2e0
Christopher Glenn <topherblair@gmail.com>
2024-08-30 21:54:44 -0700

    8/30/2024 NIGHT

---
+++ Scenes/LevelOne/level_one.gd
@@ -0,0 +1,10 @@
+extends Node2D
+# LEVEL ONE SCRIPT
+@onready var VISIBILITY_TILES = $Visibility
+
+
+func _ready():
+    pass
+
+func _physics_process(_delta):
+    pass
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -7,9 +7,8 @@ extends CharacterBody2D
 @onready var RAY_RIGHT = $Ray_RIGHT
 @onready var RAY_DOWN = $Ray_DOWN
 @onready var RAY_LEFT = $Ray_LEFT
-@onready var RAY_VIS = $Ray_VIS
-@export_group("Player Movement") # player movement group
 @export var move_timer_set : int = 20 # pause length between movement
+@export var field_of_view : int = 30 # how many tiles the player can see ahead
 var move_timer : int = 0 # paused time before player moves again
 
 
@@ -23,7 +22,7 @@ func _process(delta):
 	# updates
 	Globals.player_x = global_position.x # update player_x
 	Globals.player_y = global_position.y # update player_y
-	
+
 
 func player_input(clock):
 	# check for player input and update the game accordingly
@@ -80,10 +79,3 @@ func player_input(clock):
 	# DEBUG
 	if Input.is_action_just_pressed("ci_END"):
 		get_tree().quit() # exit the game
-	# debug visibility
-	RAY_VIS.rotation += clock * Globals.timer_ctrl
-	if RAY_VIS.is_colliding():
-		# get the collision point for layer 1 and stop the ray there
-		# get the collision tile for 3 and delete that tile
-		#print("COLLIDING")
-		pass
