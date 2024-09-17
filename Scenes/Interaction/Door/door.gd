c91f50ee273276ed72501101b9139fdfb172f936
Christopher Glenn <topherblair@gmail.com>
2024-09-17 13:26:19 -0700

    9/17/2024 NOON (door fov)
    Figured out door fov solution.

--- Scenes/Interaction/Door/door.gd
+++ Scenes/Interaction/Door/door.gd
@@ -1,31 +1,23 @@
 extends Area2D
 # DOOR SCRIPT
 # unlocked/locked, even portcullis, this script covers doors!
-@onready var SPRITE = $AnimatedSprite2D
 @export_group("Door Attributes")
 @export var door_id : String = "" # sets the door ID to search in the globals on loading
+@export var current_tilemap : TileMapLayer # set the current tilemap to manipulate
 @export_enum("wood_a", "wood_b", "portcullis", "steel") var door_type : String = "wood_a" # defaults to wood_a
 @export var opened : bool = false # if the door is opened or not
 @export var locked : bool = false # if the door is 'locked'
 @export var breakable : bool = true # if this door is breakable or not
 @export var switch_controlled : bool = false # if this door requires a switch to open it (also other unlock methods outside of the door)
 @export var hit_points : int = 5 # defaults to 5 IF breakable
-@export var defense : int = 6 # the defense (how much it could damage the player's weapon)
 
 
 func _ready():
-	# set the starting frame based on the door_type
-	match door_type:
-		"wood_a": 
-			SPRITE.frame = 0 # first wood door
-		"wood_b":
-			SPRITE.frame = 2 # second wood door
-		"portcullis":
-			SPRITE.frame = 4 # portcullis
-		"steel":
-			SPRITE.frame = 6 # steel door
-	# check if the door is opened or not and set the sprite
-	if opened: SPRITE.frame += 1 # increment to the next frame (the open frame)
+	if current_tilemap:
+		# get the current position
+		var door_position = current_tilemap.local_to_map(global_position)
+		# update the tilemap based on the door_type
+		current_tilemap.set_cell(door_position,1,Vector2i(2,5))
 
 
 func _on_body_exited(body:Node2D) -> void:
