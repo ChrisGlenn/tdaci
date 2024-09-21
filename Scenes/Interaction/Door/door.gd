4d12b570367825854c5112aee12c82e95e2f1f82
Christopher Glenn <topherblair@gmail.com>
2024-09-20 21:59:12 -0700

    9/20/2024 NIGHT

--- Scenes/Interaction/Door/door.gd
+++ Scenes/Interaction/Door/door.gd
@@ -2,6 +2,7 @@ extends Area2D
 # DOOR SCRIPT
 # unlocked/locked, even portcullis, this script covers doors!
 # door tile reference (0,5x1 2,5x3 4,5x5 10,5x11)
+@onready var INTERACT_UI = preload("res://Scenes/UI/Interaction/interaction.tscn")
 @export_group("Door Attributes")
 @export var door_id : String = "" # sets the door ID to search in the globals on loading
 @export var current_tilemap : TileMapLayer # set the current tilemap to manipulate
@@ -16,7 +17,9 @@ var door_reference_exists : bool = false # if the door is in the level_doors Glo
 var door_ref : int = 0 # based on the x coord for the tiles
 var tilemap_ref : int = 1 # defaults to 1 (solid/can't see thru)
 var frame_ref : int = 474 # the reference UI frame (defaults to black)
+var is_active : bool = false # true if the player is colliding
 var interaction_data : Dictionary = {} # this gets sent to the Interaction HUD
+var interaction_ui # holds the interaction UI
 
 
 func _ready():
@@ -89,8 +92,18 @@ func _ready():
 		current_tilemap.set_cell(door_position,tilemap_ref,Vector2i(door_ref,5))
 
 func _process(_delta: float) -> void:
-	pass
+	door_control() # door control (input)
 
+
+func door_control():
+	# check if the door is active and look for input
+	if is_active:
+		if Input.is_action_just_pressed("ci_A"):
+			if !interaction_ui:
+				var i_ui = INTERACT_UI.instantiate()
+				get_tree().root.add_child(i_ui)
+				interaction_ui = i_ui
+				print(interaction_ui)
 
 func door_update():
 	pass
@@ -99,9 +112,9 @@ func door_update():
 func _on_body_exited(body:Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		# update the HUD to let the player know they can interact with the door
-		pass
+		is_active = true # set to true
 
 func _on_body_entered(body:Node2D) -> void:
 	if body.is_in_group("PLAYER"):
 		# update the HUD the player is now out of range of the door
-		pass
+		is_active = false # set to false
--- Scenes/UI/Interaction/interaction.gd
+++ Scenes/UI/Interaction/interaction.gd
@@ -1,9 +1,24 @@
-extends Node2D
+extends CanvasLayer
 # INTERACTION SCRIPT
 # This HUD element is instanced whenever the player talks or interacts with an NPC or object.
 # Dialogue/interaction choices will be listed along with an image of what the player is interacting with.
-var description_text : Array = [] # holds the descriptions (Strings) 
+var description_text : String = "" # holds the descriptions (Strings) 
 var choices : Array = [] # holds the choices (Strings)
 var description_pos : int = 0 # the current description_text array position
 var choices_pos : int = 0 # the current choices array position
 var can_select : bool = true # used pause choice selector iteration from player
+var update : bool = false # if true then update the interaction HUD dialogue
+
+
+func _ready() -> void:
+    pass
+
+func _process(_delta: float) -> void:
+    pass
+
+
+func int_controls():
+    pass
+
+func int_update():
+    pass
